0bc6134014eed370585042a5dc5f1d252ce93c2f
ellii <patrick@savells.net>
2025-11-23 18:53:15 -0600

    Updating host_dll.hpp

---
+++ dumped/c++/host_dll.hpp
@@ -0,0 +1,26 @@
+//  dumped by https://moonlightrblx.github.io/sdkviewer
+// timestamp: 2025-11-24 00:52:15.620031200 UTC
+
+#pragma once
+
+#include <cstddef>
+
+namespace cs2_dumper {
+    namespace schemas {
+        // module: host.dll
+        // class count: 2
+        // enum count: 0
+        namespace host_dll {
+            // parent: None
+            // field count: 1
+            namespace EmptyTestScript {
+                constexpr std::ptrdiff_t m_hTest = 0x10; // CAnimScriptParam<float32>
+            }
+            // parent: None
+            // field count: 1
+            namespace CAnimScriptBase {
+                constexpr std::ptrdiff_t m_bIsValid = 0x8; // bool
+            }
+        }
+    }
+}
