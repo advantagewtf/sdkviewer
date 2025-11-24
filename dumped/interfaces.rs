5d02e8a84b02f16bce2067b7dbea162ede5897eb
ellii <patrick@savells.net>
2025-11-24 10:37:25 -0600

    Updating interfaces.rs

--- dumped/interfaces.rs
+++ dumped/interfaces.rs
@@ -1,5 +1,5 @@
 //  dumped by https://moonlightrblx.github.io/sdkviewer
-// timestamp: 2025-11-24 15:57:49.568833300 UTC
+// timestamp: 2025-11-24 16:35:41.351736700 UTC
 
 #![allow(non_upper_case_globals, unused)]
 
